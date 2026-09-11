0259209d3f6f04ad4928abf16a5a40ed0eaa646b
Prashant <prashant.92134@gmail.com>
2026-09-11 23:07:57 +0530

    last chance

--- WebDev-L3-PizzaDelivery/backend/backend/src/app.js
+++ WebDev-L3-PizzaDelivery/backend/backend/src/app.js
@@ -12,7 +12,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 import connectDB from "./config/database.js";
 import router from "./routers/auth.router.js";
-//import cors from "cors";
+import cors from "cors";
 import express from "express";
 import inventoryRouter from "./routers/inventory.router.js";
 import addressRouter from "./routers/address.router.js";
